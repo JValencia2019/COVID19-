bc1deacb0987ca8161cb92013a65a136c0b1f675
Emer Isau <emer.isau@gmail.com>
2020-05-18 22:51:47 -0500

    Se añadio vue-router

--- covid-app/src/main.js
+++ covid-app/src/main.js
@@ -1,10 +1,22 @@
+
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-
+import axios from 'axios';
+import VueRouter from 'vue-router';
 Vue.config.productionTip = false
+Vue.prototype.$http=axios;
+Vue.use(VueRouter);
+const routes = [
+  { path: '/', component:()=> import('./view/Home.vue') },
+
+]
+const router = new VueRouter({
+  routes // short for `routes: routes`
+});
 
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
